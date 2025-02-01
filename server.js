48f09c6964d8681d254465dcc91729b7a53eb8db
Bia Brauna <167824411+biabrauna@users.noreply.github.com>
2025-01-31 22:12:13 -0300

    ergefed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 
 const prisma = new PrismaClient();
 const app = express();
-const port = PORT.process.env
+const port = PORT.process.env || 3000
 
 app.use(express.json());
 app.use(cors({ origin: 'https://econsciente-app.netlify.app' }));
