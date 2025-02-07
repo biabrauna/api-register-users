b993e90e5fc5dd8fed689b9f5a7460f21685dab8
Bia Brauna <167824411+biabrauna@users.noreply.github.com>
2025-02-07 10:13:32 -0300

    cors no topo

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const prisma = new PrismaClient();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
